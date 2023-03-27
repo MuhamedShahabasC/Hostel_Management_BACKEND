0e5a24f6748f2c9aa1656f54c7434b0cc7fc5dc3
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-03-27 15:21:15 +0530

    Authentication service

--- dist/services/auth.js
+++ dist/services/auth.js
@@ -1,22 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// Unified authentication service for everyone
-// export type AuthRoles = "student" | "staff" | "chief warden";
-// export abstract class AuthService {
-//   abstract role: AuthRoles;
-//   abstract find(email: string): Promise<IChiefWarden | null>;
-//   async login(email: string, password: string): Promise<IChiefWarden> {
-//     const existingUser = await this.find(email.toLowerCase());
-//     if (!existingUser) {
-//       throw ErrorResponses.noDataFound(this.role);
-//     }
-//     const validPassword = await comparePassword(
-//       password,
-//       existingUser.password
-//     );
-//     if (!validPassword) {
-//       throw ErrorResponses.unautharized("Invalid Password");
-//     }
-//     return existingUser;
-//   }
-// }
+exports.AuthService = void 0;
+const ErrorResponses_1 = __importDefault(require("../error/ErrorResponses"));
+const passwordManager_1 = require("../utils/passwordManager");
+class AuthService {
+    async login(email, password) {
+        const existingUser = await this.find(email.toLowerCase());
+        if (!existingUser) {
+            throw ErrorResponses_1.default.noDataFound(this.role);
+        }
+        const validPassword = await (0, passwordManager_1.comparePassword)(password, existingUser.password);
+        if (!validPassword) {
+            throw ErrorResponses_1.default.unautharized("Invalid Password");
+        }
+        return existingUser;
+    }
+}
+exports.AuthService = AuthService;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,24 +4,24 @@ import { comparePassword } from "../utils/passwordManager";
 
 // Unified authentication service for everyone
 
-// export type AuthRoles = "student" | "staff" | "chief warden";
+export type AuthRoles = "student" | "staff" | "chief warden";
 
-// export abstract class AuthService {
-//   abstract role: AuthRoles;
-//   abstract find(email: string): Promise<IChiefWarden | null>;
+export abstract class AuthService {
+  abstract role: AuthRoles;
+  abstract find(email: string): Promise<IChiefWarden | null>;
 
-//   async login(email: string, password: string): Promise<IChiefWarden> {
-//     const existingUser = await this.find(email.toLowerCase());
-//     if (!existingUser) {
-//       throw ErrorResponses.noDataFound(this.role);
-//     }
-//     const validPassword = await comparePassword(
-//       password,
-//       existingUser.password
-//     );
-//     if (!validPassword) {
-//       throw ErrorResponses.unautharized("Invalid Password");
-//     }
-//     return existingUser;
-//   }
-// }
+  async login(email: string, password: string): Promise<IChiefWarden> {
+    const existingUser = await this.find(email.toLowerCase());
+    if (!existingUser) {
+      throw ErrorResponses.noDataFound(this.role);
+    }
+    const validPassword = await comparePassword(
+      password,
+      existingUser.password
+    );
+    if (!validPassword) {
+      throw ErrorResponses.unautharized("Invalid Password");
+    }
+    return existingUser;
+  }
+}
