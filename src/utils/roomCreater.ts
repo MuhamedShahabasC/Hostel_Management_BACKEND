68791c19667b6c3c3e8bf6b0984f44484c7f375b
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-03-29 21:09:57 +0530

    Room creator module

--- src/utils/roomCreater.ts
+++ src/utils/roomCreater.ts
@@ -1,17 +1,17 @@
-// // Create required number of room objects in a block
+// Create required number of room objects in a given block
 
-// export const roomCreater = (
-//   blockCode: string,
-//   numberOfRooms: number
-// ): { number: number; code: string }[] => {
-//   const roomArray = [];
-//   let currentRoom = 1;
-//   while (currentRoom <= numberOfRooms) {
-//     roomArray.push({
-//       number: currentRoom,
-//       code: `${blockCode}${currentRoom < 10 ? "0" + currentRoom : currentRoom}`,
-//     });
-//     currentRoom++;
-//   }
-//   return roomArray;
-// };
+export const roomCreater = (
+  blockCode: string,
+  numberOfRooms: number
+): { number: number; code: string }[] => {
+  const roomArray = [];
+  let currentRoom = 1;
+  while (currentRoom <= numberOfRooms) {
+    roomArray.push({
+      number: currentRoom,
+      code: `${blockCode}${currentRoom < 10 ? "0" + currentRoom : currentRoom}`,
+    });
+    currentRoom++;
+  }
+  return roomArray;
+};
