dce9e7aaaeb98f621f1bc619a67785ce51eb6a96
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-03-30 07:34:15 +0530

    Notice Service

--- src/models/notice.ts
+++ src/models/notice.ts
@@ -1,37 +1,37 @@
-// import { model, Model, Schema } from "mongoose";
-// import { INotice } from "../interfaces/chiefWarden";
+import { model, Model, Schema } from "mongoose";
+import { INotice } from "../interfaces/chiefWarden";
 
-// const noticeSchema = new Schema<INotice>({
-//   title: {
-//     type: String,
-//     required: [true, "Notice must have a name"],
-//   },
-//   message: {
-//     type: String,
-//     required: [true, "Notice must have a message"],
-//     maxlength: [200, "Maximum 200 characters"],
-//   },
-//   audience: {
-//     type: {
-//       student: {
-//         type: Boolean,
-//         required: [true, "Specify audience"],
-//       },
-//       staff: {
-//         type: Boolean,
-//         required: [true, "Specify audience"],
-//       },
-//     },
-//     required: [true, "Notice must have a audience"],
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now(),
-//   },
-//   visibility: {
-//     type: Boolean,
-//     default: true,
-//   },
-// });
+const noticeSchema = new Schema<INotice>({
+  title: {
+    type: String,
+    required: [true, "Notice must have a name"],
+  },
+  message: {
+    type: String,
+    required: [true, "Notice must have a message"],
+    maxlength: [200, "Maximum 200 characters"],
+  },
+  audience: {
+    type: {
+      student: {
+        type: Boolean,
+        required: [true, "Specify audience"],
+      },
+      staff: {
+        type: Boolean,
+        required: [true, "Specify audience"],
+      },
+    },
+    required: [true, "Notice must have a audience"],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  visibility: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-// export const NoticeModel: Model<INotice> = model("Notice", noticeSchema);
+export const NoticeModel: Model<INotice> = model("Notice", noticeSchema);
--- src/services/notice.ts
+++ src/services/notice.ts
@@ -1,24 +1,33 @@
-// import { INotice } from "../interfaces/chiefWarden";
-// import { NoticeRepo } from "../repositories/notice";
+import { INotice } from "../interfaces/chiefWarden";
+import { NoticeRepo } from "../repositories/notice";
 
-// export class NoticeService extends NoticeRepo {
-//     async singleNotice(_id: string){
-//         return await this.single(_id)
-//     }
+
+// Notice Service
+
+export class NoticeService extends NoticeRepo {
     
-//     async updateNotice (_id: string, data: INotice){
-//        return await this.update(_id, data)
-//     }
+    // Post new notice
+    async newNotice(data: INotice){
+        return await this.post(data)
+    }
 
-//     async newNotice(data: INotice){
-//         return await this.post(data)
-//     }
+    // Get single notice details
+    async singleNotice(_id: string){
+        return await this.single(_id)
+    }
+    
+    // Update new notice
+    async updateNotice (_id: string, data: INotice){
+       return await this.update(_id, data)
+    }
 
-//     async changeVisibility(_id: string, newVisibility:boolean){
-//         return await this.update(_id, {visibility: newVisibility})
-//     }
+    // Show / Hide a notice
+    async changeVisibility(_id: string, newVisibility:boolean){
+        return await this.update(_id, {visibility: newVisibility})
+    }
 
-//     async deleteNotice(_id: string){
-//         return await this.remove(_id)
-//     }
-// }
+    // Delete a notice
+    async deleteNotice(_id: string){
+        return await this.remove(_id)
+    }
+}
