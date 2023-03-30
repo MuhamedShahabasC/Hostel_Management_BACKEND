8af84a482724844da7b6d35eaaf930becfd2f270
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-03-30 07:32:13 +0530

    Notice Repository

--- src/repositories/notice.ts
+++ src/repositories/notice.ts
@@ -1,24 +1,26 @@
-// import { NoticeModel } from "../models/notice";
-// import { CRUD } from "./CRUD";
-// import { INotice } from "../interfaces/chiefWarden";
-// import { Model } from "mongoose";
+import { NoticeModel } from "../models/notice";
+import { CRUD } from "./CRUD";
+import { INotice } from "../interfaces/chiefWarden";
+import { Model } from "mongoose";
 
-// export class NoticeRepo extends CRUD {
-//     model: Model<INotice> = NoticeModel;
-//    protected async update(_id: string, data: INotice){
-//     console.log(data)
-//         return await this.idAndUpdate(_id,data)
-//     }
+// Notice Repository
 
-//     protected async post(data: INotice){
-//         return await this.create(data)
-//     }
+export class NoticeRepo extends CRUD {
+  model: Model<INotice> = NoticeModel;
+  protected async update(_id: string, data: INotice) {
+    console.log(data);
+    return await this.idAndUpdate(_id, data);
+  }
 
-//     protected async single(_id: string){
-//         return await this.findOne({_id})
-//     }
+  protected async post(data: INotice) {
+    return await this.create(data);
+  }
 
-//     protected async remove(_id: string){
-//         return await this.idAndDelete({_id})
-//     }
-// }
+  protected async single(_id: string) {
+    return await this.findOne({ _id });
+  }
+
+  protected async remove(_id: string) {
+    return await this.idAndDelete({ _id });
+  }
+}
