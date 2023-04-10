51cb55173ac54358f9679f47662da7f23328fa18
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-04-10 16:18:19 +0530

    Token verification

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -5,12 +5,10 @@ import validator from "validator";
 import { IToken } from "../interfaces/auth";
 
 export const checkAuth = asyncHandler(async (req, res, next) => {
-  if (!req.headers?.authorization)
-    throw ErrorResponses.unauthorized("Access Denied");
-  const token = req.headers.authorization.replace("Bearer ", "");
-  const { _id } = verifyToken(token) as IToken;
-  if (!_id || !validator.isMongoId(_id)) throw ErrorResponses.badRequest();
-  next()
+    if (!req.headers?.authorization)
+      throw ErrorResponses.unauthorized("Access Denied");
+    const token = req.headers.authorization.replace("Bearer ", "");
+    const { _id } = verifyToken(token) as IToken;
+    if (!_id || !validator.isMongoId(_id)) throw ErrorResponses.badRequest();
+    next();
 });
-
-
--- src/routes/utility.ts
+++ src/routes/utility.ts
@@ -12,10 +12,14 @@ export const endPointNotFound = asyncHandler(async (req, res) => {
 
 // Verify authorization headers
 export const verifyAuth = asyncHandler(async (req, res) => {
-  if (!req.headers?.authorization)
+  try {
+    if (!req.headers?.authorization)
+      throw ErrorResponses.unauthorized("Access Denied");
+    const token = req.headers.authorization.replace("Bearer ", "");
+    const { _id } = verifyToken(token) as IToken;
+    if (!_id || !validator.isMongoId(_id)) throw ErrorResponses.badRequest();
+    res.json(dataFormattor("Authorized User"));
+  } catch (error) {
     throw ErrorResponses.unauthorized("Access Denied");
-  const token = req.headers.authorization.replace("Bearer ", "");
-  const { _id } = verifyToken(token) as IToken;
-  if (!_id || !validator.isMongoId(_id)) throw ErrorResponses.badRequest();
-  res.json(dataFormattor("Authorized User"));
+  }
 });
